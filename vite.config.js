fdba07259d28fcac837919f230444b94a1eaa536
RajiNair GitHub <raji@gmail.com>
2024-12-24 01:45:07 -0700

    did new changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Rajashree_Portfolio/', // Use your exact repository name here
-})
+  base: '/Rajashree_Portfolio/', // Correct base URL for GitHub Pages
+});
+
