a41065b92310069b61d58079767376d5e0917d81
RajiNair GitHub <raji@gmail.com>
2024-12-24 02:31:35 -0700

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/Rajashree_Portfolio/', // Correct base URL for GitHub Pages
-});
+
+export default {
+  base: '/Rajashree_Portfolio/', // This ensures that paths are correctly resolved during deployment
+};
 
