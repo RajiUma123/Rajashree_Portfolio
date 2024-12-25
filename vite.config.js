0c27ce98a3cfc24a2a905427bfaedd69a5c6e2ba
Rajashree Nair <99322623+RajiUma123@users.noreply.github.com>
2024-12-24 20:32:27 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Rajashree_Portfolio/', // Use your exact repository name here
+  base: '/', // Use your exact repository name here
 })
