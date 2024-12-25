d339b66c99ed12fba3698cce9e58df92121dc8b9
Rajashree Nair <99322623+RajiUma123@users.noreply.github.com>
2024-12-25 09:03:30 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.jsx"; // Ensure this import has the correct extension
+import App from "./App.js"; // Ensure this import has the correct extension
 import './index.css'
 ReactDOM.render(
   <React.StrictMode>
